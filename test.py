6c787d56bbb1bf38e4634be94240783b09fec5c9
Peanid <thenewpernicus@gmail.com>
2022-01-24 19:13:05 +0700

    you beauty

---
+++ test.py
@@ -0,0 +1,9 @@
+a = 1
+b = "hello"
+print(b)
+for i in range(10):
+    print(i)
+def plobsheim_sex(gods, numba):
+    for i in range(numba):
+        print(f"{gods} he's got that {i}")
+plobsheim_sex("fart", 50)
