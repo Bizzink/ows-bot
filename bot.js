c91069d7e09c02e382f9397e7293ef4c76819e55
Bizzink <bizink@pobnellion.com>
2022-01-29 02:29:40 +1300

    We get the message

--- bot.js
+++ bot.js
@@ -2,12 +2,22 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// Create a new client instance with permissions
+const client = new Client({ intents: [
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MESSAGES, 
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+// When a message gets sent
+client.on("messageCreate", message => {
+    //console.log(message);
+    console.log(`${message.author.username}: ${message.content}`);
 });
 
 // Login to Discord with your client's token
